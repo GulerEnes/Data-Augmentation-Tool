f2a4699488f950ec7a18a77bf30305ea956a9339
Enes Güler <kriptoenes@gmail.com>
2022-02-07 22:49:17 +0300

    [Bug fix] multchoicebox item limit, module name correction

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import cv2 as cv
-import numpy as np
 import easygui as eg
 from os.path import splitext
 from os import walk
@@ -28,8 +26,15 @@ içine ayrı ayrı kaydet. Ayrıca orijinaller için de bir klasör aç
 
 directory = eg.diropenbox(msg="Choose directory", title="Directory select box", default='.')
 
-fileExtList = utils.Other.file_extentions(directory)
-fileExt = eg.multchoicebox(msg="Select file extentions", title="Select file extentions", choices=list(fileExtList))
+fileExtList = list(utils.Other.file_extentions(directory))
+
+if len(fileExtList) == 0:
+	raise "There are no file here!"
+if len(fileExtList) == 1:
+	fileExtList.append('')  # Dummy item, because multchoicebox need at least 2 item
+
+
+fileExt = eg.multchoicebox(msg="Select file extentions", title="Select file extentions", choices=fileExtList)
 
 choices = [i for i in dir(utils.DataAugmentation) if i[0] != '_']
 
--- utils.py
+++ utils.py
@@ -1,6 +1,6 @@
-import cv2 as _cv
-import os as _os
-import skimage as _skimage
+import cv2 as cv
+import os as os
+import skimage as skimage
 
 
 class DataAugmentation:
@@ -12,74 +12,72 @@ class DataAugmentation:
 
 	def contrast(self, image, alpha, beta):
 		# alpha value [1.0-3.0], beta value [0-100]
-		return _cv.convertScaleAbs(image, alpha=alpha, beta=beta)
+		return cv.convertScaleAbs(image, alpha=alpha, beta=beta)
 
 	def flip_horizontal(self, img):
-		return _cv.flip(img, 1)
+		return cv.flip(img, 1)
 
 	def flip_vertical(self, img):
-		return _cv.flip(img, 0)
+		return cv.flip(img, 0)
 
 	def flip_origin(self, img):
-		return _cv.flip(img, -1)
+		return cv.flip(img, -1)
 
 	def noise(self, img):
-		return _skimage.util.random_noise(img, mode="gaussian")
+		return skimage.util.random_noise(img, mode="gaussian")
 
 	def rotate_90_clockwise(self, img):
-		return _cv.rotate(img, _cv.ROTATE_90_CLOCKWISE)
+		return cv.rotate(img, cv.ROTATE_90_CLOCKWISE)
 
 	def rotate_90_counter_clockwise(self, img):
-		return _cv.rotate(img, _cv.ROTATE_90_COUNTERCLOCKWISE)
+		return cv.rotate(img, cv.ROTATE_90_COUNTERCLOCKWISE)
 
 	def brightness(self, img, value=0):
-		hsv = _cv.cvtColor(img, _cv.COLOR_BGR2HSV)
-		h, s, v = _cv.split(hsv)
-		v = _cv.add(v, value)
+		hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
+		h, s, v = cv.split(hsv)
+		v = cv.add(v, value)
 		v[v > 255] = 255
 		v[v < 0] = 0
-		final_hsv = _cv.merge((h, s, v))
-		img = _cv.cvtColor(final_hsv, _cv.COLOR_HSV2BGR)
+		final_hsv = cv.merge((h, s, v))
+		img = cv.cvtColor(final_hsv, cv.COLOR_HSV2BGR)
 		return img
 
 	def saturation(self, img, value=0):
-		hsv = _cv.cvtColor(img, _cv.COLOR_BGR2HSV)
-		h, s, v = _cv.split(hsv)
-		s = _cv.add(s, value)
+		hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
+		h, s, v = cv.split(hsv)
+		s = cv.add(s, value)
 		s[s > 255] = 255
 		s[s < 0] = 0
-		final_hsv = _cv.merge((h, s, v))
-		img = _cv.cvtColor(final_hsv, _cv.COLOR_HSV2BGR)
+		final_hsv = cv.merge((h, s, v))
+		img = cv.cvtColor(final_hsv, cv.COLOR_HSV2BGR)
 		return img
 
 	def hue(self, img, value=0):
-		hsv = _cv.cvtColor(img, _cv.COLOR_BGR2HSV)
-		h, s, v = _cv.split(hsv)
-		h = _cv.add(h, value)
+		hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
+		h, s, v = cv.split(hsv)
+		h = cv.add(h, value)
 		h[h > 255] = 255
 		h[h < 0] = 0
-		final_hsv = _cv.merge((h, s, v))
-		img = _cv.cvtColor(final_hsv, _cv.COLOR_HSV2BGR)
+		final_hsv = cv.merge((h, s, v))
+		img = cv.cvtColor(final_hsv, cv.COLOR_HSV2BGR)
 		return img
 
 	def grayScale(self, img):
-		return _cv.cvtColor(img, _cv.COLOR_BGR2GRAY)
+		return cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
 
 class Other:
 	@staticmethod
 	def file_extentions(directory):
 		fileExts = set()
-		_, _, filenames = next(_os.walk(directory))
+		_, _, filenames = next(os.walk(directory))
 
 		for f in filenames:
-			_, ext = _os.path.splitext(f)
+			_, ext = os.path.splitext(f)
 			fileExts.add(ext)
 
-		try:  # deleting directory extension if exists
+		if '' in fileExts:  # deleting directory extension if exists
 			fileExts.remove('')
-		except Exception as e:
-			print("Error:", e)
 
 		return fileExts
 
